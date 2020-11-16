308914cc7f8bf1cfd649a3d9d2076fe051ce5b3c
daishitie <daishitie@gmail.com>
2020-11-17 01:54:45 +0800

    fixed errors

--- app/init.php
+++ app/init.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_type=1);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1); 
+declare(strict_types=1);
+ini_set('display_errors', 'true');
+ini_set('display_startup_errors', 'true'); 
 error_reporting(E_ALL);
-
 
 function config(string $key)
 {
--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 
 require_once __DIR__ . '/app/init.php';
 
+print_r('Sample config(\'app.name\') value: ');
 print_r(config('app.name'));
